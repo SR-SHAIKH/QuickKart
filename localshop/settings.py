253dfafaf7d488463d723d48bfdceb97fb907142
SR SHAIKH <shaikhsohel.edu@gmail.com>
2025-07-21 20:36:27 +0530

    21/07/2025

--- localshop/settings.py
+++ localshop/settings.py
@@ -74,6 +74,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT", default="5432"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
