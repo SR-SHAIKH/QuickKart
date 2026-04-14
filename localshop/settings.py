61bb966cfa5cd35ca7786e6146aa12ddde4fbf52
SR SHAIKH <shaikhsohel.edu@gmail.com>
2026-04-14 16:49:23 +0530

    fix: static storage to prevent admin 500

--- localshop/settings.py
+++ localshop/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media files
 MEDIA_URL = "/media/"
