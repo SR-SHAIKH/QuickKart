1271cf1e46ed2edf8ee9f9805147d03169eecc2b
SR SHAIKH <shaikhsohel.edu@gmail.com>
2026-04-14 17:14:11 +0530

    admin auth

--- localshop/settings.py
+++ localshop/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media files
