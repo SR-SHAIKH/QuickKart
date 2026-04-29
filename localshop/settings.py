9698db95d455abe42757ab24c549bd2efd4aa77f
SR SHAIKH <shaikhsohel.edu@gmail.com>
2026-04-29 13:04:58 +0530

    fix: enforce os.path.join for MEDIA_ROOT

--- localshop/settings.py
+++ localshop/settings.py
@@ -131,7 +131,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media files
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
